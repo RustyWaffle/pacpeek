e3fdf7c25963695fb5871b09c85620f44425b0c6
RustyWaffle <rustywaffle@seznam.cz>
2026-07-19 11:47:28 +0200

    Added hardcoded file reading line by line on firefox.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::{self, Read};
+use std::fs::File;
+
+fn main() ->io::Result<()> {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    for argument in args.iter(){
+        println!("Searching for: {}", argument)
+    }
+
+    //open
+    let mut file = File::open("/var/lib/pacman/local/firefox-152.0.6-1/desc")?;
+
+    //init string
+    let mut content: String = String::new();
+
+    //read
+    file.read_to_string(&mut content)?;
+
+    println!("{}", content);
+    
+    Ok(())
 }
